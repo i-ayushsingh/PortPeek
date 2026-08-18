3b1dac9eb0f4c1eab9febd2129f6cc19c56b2a13
i-ayushsingh <ayushsingh.cse25@gmail.com>
2026-08-19 02:54:14 +0530

    ci: add setup-nsis action in GitHub Actions and fix makensis path resolution

--- build_installer.bat
+++ build_installer.bat
@@ -5,22 +5,30 @@ echo ============================================================
 echo  Building PortPeek NSIS Installer
 echo ============================================================
 
-set "MAKENSIS=C:\Program Files (x86)\NSIS\makensis.exe"
-if not exist "%MAKENSIS%" (
-    set "MAKENSIS=C:\Program Files\NSIS\makensis.exe"
-)
-if not exist "%MAKENSIS%" (
-    where makensis.exe >nul 2>&1
-    if %ERRORLEVEL% EQU 0 set "MAKENSIS=makensis.exe"
-)
+set "MAKENSIS="
 
-if not exist "%MAKENSIS%" (
-    if "%MAKENSIS%" NEQ "makensis.exe" (
-        echo [!] NSIS compiler not found at standard path.
-        exit /b 1
+if exist "C:\Program Files (x86)\NSIS\makensis.exe" (
+    set "MAKENSIS=C:\Program Files (x86)\NSIS\makensis.exe"
+) else if exist "C:\Program Files\NSIS\makensis.exe" (
+    set "MAKENSIS=C:\Program Files\NSIS\makensis.exe"
+) else if exist "C:\ProgramData\chocolatey\bin\makensis.exe" (
+    set "MAKENSIS=C:\ProgramData\chocolatey\bin\makensis.exe"
+) else (
+    where makensis.exe >nul 2>&1
+    if %ERRORLEVEL% EQU 0 (
+        for /f "delims=" %%I in ('where makensis.exe') do (
+            if not defined MAKENSIS set "MAKENSIS=%%I"
+        )
     )
 )
 
+if not defined MAKENSIS (
+    echo [!] NSIS compiler makensis.exe was not found on this system.
+    echo [!] Please install NSIS from https://nsis.sourceforge.io
+    exit /b 1
+)
+
+echo [*] Using NSIS compiler: "%MAKENSIS%"
 echo [*] Compiling installer with NSIS...
 "%MAKENSIS%" /V3 "installer.nsi"
 
